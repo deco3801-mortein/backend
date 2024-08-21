e2b8af51094ff3629aa63fed1adc4328a84c544f
William Sawyer <wmsawyer2609@gmail.com>
2024-08-21 15:23:58 +0000

    Initialise Swagger integration

--- Mortein/Program.cs
+++ Mortein/Program.cs
@@ -1,5 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHttpsRedirection();
+}
 
 app.MapGet("/", () => "Hello World!");
 
