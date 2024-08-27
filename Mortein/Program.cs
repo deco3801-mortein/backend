dd8acd75354305d09a34848d4c22f961c3355f4a
William Sawyer <wmsawyer2609@gmail.com>
2024-08-27 14:58:34 +0000

    Configure API docs document title

--- Mortein/Program.cs
+++ Mortein/Program.cs
@@ -24,6 +24,7 @@ if (app.Environment.IsDevelopment())
     });
     app.UseSwaggerUI(c =>
     {
+        c.DocumentTitle = "Mortein API Docs";
         c.RoutePrefix = "docs";
         c.SwaggerEndpoint("/openapi.json", "Mortein API");
     });
