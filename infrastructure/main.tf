7846dabc25a3f977c35953f4763a87efa7bdd755
William Sawyer <wmsawyer2609@gmail.com>
2024-09-04 13:47:42 +0000

    Initialise AWS Terraform provider

---
+++ infrastructure/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+  }
+
+  required_version = "~> 1.9"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
