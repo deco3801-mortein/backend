cef989846ae87b8c277f481ba3eaad04b776d121
William Sawyer <wmsawyer2609@gmail.com>
2024-09-13 12:16:04 +0000

    Initialise device model

--- Mortein/DatabaseContext.cs
+++ Mortein/DatabaseContext.cs
@@ -1,10 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using Mortein.Models;
 
 namespace Mortein;
 
 /// <inheritdoc/>
 public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
 {
+    /// <summary>
+    /// Interface for interacting with devices in the database.
+    /// </summary>
+    public DbSet<Device> Devices { get; set; } = null!;
+
     /// <inheritdoc/>
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
---
+++ Mortein/Models/Device.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mortein.Models;
+
+/// <summary>
+/// Pest-deterrent device.
+/// </summary>
+[Table("Device")]
+public class Device
+{
+    /// <summary>
+    /// The unique identifier of a device.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The name of a device.
+    /// </summary>
+    public required string Name { get; set; }
+}
